d54c47f9b4c0e69ee3f13470f02efba53261a60b
ZeGuilhermeYung <jgryung@gmail.com>
2022-09-25 13:54:43 -0300

    mongoClient e gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
 const server = express();
 server.use(cors());
 server.use(json());
+
+server.listen(5000);
