74d775ee7658a57050a831149ef60aa1a541a33f
ZeGuilhermeYung <jgryung@gmail.com>
2022-09-25 20:47:25 -0300

    post poll

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
+import joi from "joi";
+import dayjs from 'dayjs';
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
@@ -10,5 +12,50 @@ let db;
 const server = express();
 server.use(cors());
 server.use(json());
+
+(async () => {
+  try {
+      await mongoClient.connect();
+      db = mongoClient.db("antiBozo");
+  } catch (error) {
+      console.error(error);
+  }
+})();
+
+server.post("/poll", async (req, res) => {
+  
+  const { title, expireAt } = req.body;
+
+  const postPollSchema = joi.object({
+    title: joi.string().required(),
+    expireAt: joi.string().allow("").required(),
+  });
+
+  const validation = postPollSchema.validate({ title, expireAt }, { abortEarly: false });
+  let newExpirationDate = "";
+
+  if (validation.error) {
+    res.status(422).send("O título da enquete não pode ser vazio");
+    return;
+  }
+  if (!expireAt) {
+    console.log(Date.now());
+    newExpirationDate = dayjs(Date.now()).add(30, 'day').format("YYYY-MM-DD HH:mm");
+  } else {
+    newExpirationDate = expireAt;
+  }
+  try {
+    await db.collection("poll").insertOne({
+      title: title,
+      expireAt: newExpirationDate
+    });
+    console.log(newExpirationDate);
+    res.sendStatus(201);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Requisição incompleta, verifique os dados enviados");
+  }
+});
 
 server.listen(5000);
