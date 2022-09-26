3ea1f779b224b074d27433ac75b2d0438860b169
ZeGuilhermeYung <jgryung@gmail.com>
2022-09-25 21:52:57 -0300

    post choices

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from 'dotenv';
 import joi from "joi";
 import dayjs from 'dayjs';
@@ -66,5 +66,55 @@ server.get("/poll", async (req, res) => {
     res.status(500).send("Resposta incompleta, verifique os dados solicitados");
   }
 });
+
+server.post("/choice", async (req, res) => {
+  const { title, pollId } = req.body;
+
+  const postChoiceSchema = joi.object({
+    title: joi.string().required(),
+    pollId: joi.string().required(),
+  });
+
+  const validation = postChoiceSchema.validate({ title, pollId }, { abortEarly: false });
+  
+  if (validation.error) {
+    res.status(422).send("A opção de votação não pode ser vazia.");
+    return;
+  }
+
+  try {
+    const poll = await db.collection("poll").findOne({ _id: ObjectId(pollId) });
+
+    const choices = await db.collection("choice").find({
+      pollId: ObjectId(pollId)
+    }).toArray();
+
+    const matchedTitle = choices.find((choice) => choice.title === title);
+
+    if (!poll) {
+      res.status(404).send("Opção de enquete inexistente.");
+      return;
+    }
+    if (matchedTitle) {
+      res.status(409).send("Opção de votação já existente.");
+      return;
+    }
+    if (dayjs(poll.expireAt).valueOf() < Date.now()) {
+      res.status(403).send("Data de enquete já expirada.");
+      return;
+    }
+
+    await db.collection("choice").insertOne({
+      title,
+      pollId: ObjectId(pollId)
+    });
+    res.sendStatus(201);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Requisição incompleta, verifique os dados enviados");
+  }
+});
+
 
 server.listen(5000);
